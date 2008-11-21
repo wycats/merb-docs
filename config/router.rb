da4f4bbfa01b21b0f5325b9c67edb5aa06be2d89
Yehuda Katz <wycats@gmail.com>
2008-11-21 12:02:52 -0800

    Make / point to the list of classes

--- config/router.rb
+++ config/router.rb
@@ -38,6 +38,7 @@ Merb::Router.prepare do
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
   # clients from calling your create or destroy actions with a GET
+  match("/").to(:controller => :klasses)
   default_routes
   
   # Change this for your home page to be available at /
